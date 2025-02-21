471a56df52694da7b060abb40c76511dd9d23949
Pratham kothari <en20it301079@medicaps.ac.in>
2025-02-21 09:37:57 +0530

    port correction

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,2 +1,3 @@
 //export const BASE_URL="https://devtinder-ulzf.onrender.com"
-export const BASE_URL="http://localhost:7777"
+export const BASE_URL=
+       location.hostname==="localhost" ? "http://localhost:7777" : "https://devtinder-ulzf.onrender.com"
