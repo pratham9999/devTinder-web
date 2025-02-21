d1decf5d6120814dc4e4fabb3909b14336240ae1
Pratham kothari <en20it301079@medicaps.ac.in>
2025-02-21 09:45:02 +0530

    url-updation

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,3 +1,3 @@
 //export const BASE_URL="https://devtinder-ulzf.onrender.com"
-export const BASE_URL=
-       location.hostname==="localhost" ? "http://localhost:7777" : "https://devtinder-ulzf.onrender.com"
+export const BASE_URL = 
+       window.location.origin.includes("localhost") ? "http://localhost:7777" : "https://devtinder-ulzf.onrender.com";
